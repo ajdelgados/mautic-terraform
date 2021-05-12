1dc4a641f4c236d56c9707e1e3ed0d6997a22fce
Arturo J. Delgado S <ajdelgados@gmail.com>
2021-05-12 06:45:10 -0400

    Added eip and ec2_om

--- ec2_om/main.tf
+++ ec2_om/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "tf-aws-us-east-1.om"
-    key    = "ec2_mautic/terraform.tfstate"
+    key    = "ec2_om/terraform.tfstate"
     region = "us-east-1"
   }
 }
@@ -26,27 +26,27 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "terraform-instance" {
+resource "aws_instance" "om-instance" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
   key_name        = "LlaveAWS"
   security_groups = ["mautic-general"]
 
   tags = {
-    Name = "terraform-instance"
+    Name = "om"
   }
 
   volume_tags = {
-    Name = "terraform-instance"
+    Name = "om"
   }
 }
 
-resource "aws_eip" "lb" {
-  instance = aws_instance.web.id
+resource "aws_eip" "om-eip" {
+  instance = aws_instance.om-instance.id
   vpc      = true
 }
 
-resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
+resource "aws_cloudwatch_metric_alarm" "om-metric-cpu" {
   alarm_name                = "cpu-utilization"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "2"
@@ -57,11 +57,11 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
   threshold                 = "60"
   alarm_description         = "This metric monitors ec2 cpu utilization"
   insufficient_data_actions = []
-  dimensions                = { InstanceId = aws_instance.terraform-instance.id }
+  dimensions                = { InstanceId = aws_instance.om-instance.id }
   alarm_actions = ["arn:aws:sns:us-east-2:579010345876:mautic-alert"]
 }
 
-resource "aws_cloudwatch_metric_alarm" "ec2_fail" {
+resource "aws_cloudwatch_metric_alarm" "om-check-fail" {
   alarm_name                = "check-fail"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "2"
@@ -72,12 +72,12 @@ resource "aws_cloudwatch_metric_alarm" "ec2_fail" {
   threshold                 = "0.99"
   alarm_description         = "This metric monitors ec2 fail"
   insufficient_data_actions = []
-  dimensions                = { InstanceId = aws_instance.terraform-instance.id }
+  dimensions                = { InstanceId = aws_instance.om-instance.id }
   alarm_actions = ["arn:aws:sns:us-east-2:579010345876:mautic-alert"]
 }
 
-resource "aws_dlm_lifecycle_policy" "terraform-instance" {
-  description        = "terraform-instance DLM lifecycle policy"
+resource "aws_dlm_lifecycle_policy" "om-lcp" {
+  description        = "om-lcp DLM lifecycle policy"
   execution_role_arn = "arn:aws:iam::579010345876:role/service-role/AWSDataLifecycleManagerDefaultRole"
   state              = "ENABLED"
 
@@ -98,14 +98,14 @@ resource "aws_dlm_lifecycle_policy" "terraform-instance" {
       }
 
       tags_to_add = {
-        Name = "terraform-instance"
+        Name = "om-instance"
       }
 
       copy_tags = false
     }
 
     target_tags = {
-      Name = "terraform-instance"
+      Name = "om-instance"
     }
   }
 }
