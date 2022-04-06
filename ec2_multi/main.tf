e9b714dff1c18c7f598e6a230bd9c5d5af93982a
Arturo J. Delgado S <ajdelgados@gmail.com>
2022-04-06 19:39:15 -0400

    chore: added instances

--- ec2_multi/main.tf
+++ ec2_multi/main.tf
@@ -30,8 +30,23 @@ module "mautic-instance" {
   #data.aws_ami.ubuntu.id
   for_each = {
     orquestaagency = {
+      ami = "ami-0b29b6e62f2343b46"
+    },
+    popayan = {
+      ami = "ami-0b29b6e62f2343b46"
+    },
+    implanet = {
+      ami = "ami-0117d177e96a8481c"
+    },
+    mydesk = {
+      ami = "ami-0117d177e96a8481c"
+    },
+    votella = {
+      ami = "ami-039af3bfc52681cd5"
+    },
+    wowpets = {
       ami = data.aws_ami.ubuntu.id
-    }
+    },
   }
   source = "../modules"
   name = "${each.key}-instance"
